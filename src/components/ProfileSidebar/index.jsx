4a419cd63a764ed887b4c6fdd12751a41114a72a
Samanta Fluture <samantafluture@gmail.com>
2021-07-12 20:02:43 -0300

    add link to my github profile

--- src/components/ProfileSidebar/index.jsx
+++ src/components/ProfileSidebar/index.jsx
@@ -3,10 +3,12 @@ import Box from "../Box";
 function ProfileSidebar(props) {
   return (
     <Box>
-      <img
-        src={`https://github.com/${props.username}.png`}
-        style={{ borderRadius: "8px" }}
-      />
+      <a href={`https://github.com/${props.username}`}>
+        <img
+          src={`https://github.com/${props.username}.png`}
+          style={{ borderRadius: "8px" }}
+        />
+      </a>
     </Box>
   );
 }
